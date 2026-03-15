beee8e8e4294a84ae2877b39f2fc84421e59088b
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-03-15 18:55:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// connect to site 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
